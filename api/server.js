ae5e5e35d9a72b15c66b3a0b96e3cd2b17a9b6ea
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-11 13:55:10 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const accountsRouter = require("./api/accounts/accounts-router");
 
-const server = express();
+const app = express();
 
-server.use(express.json());
+app.use(express.json());
 
-module.exports = server;
+app.use("/api/accounts", accountsRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err); // Log the error for debugging purposes
+  res.status(500).json({ message: "Something went wrong" });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
