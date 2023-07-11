28f89d0cb2de04911a29140bdbdaff23b091d86b
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-11 13:52:16 -0700

    Done

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -25,21 +25,58 @@ router.get("/:id", checkAccountId, async (req, res, next) => {
   }
 });
 
-router.post("/", (req, res, next) => {
-  // DO YOUR MAGIC
-});
+router.post(
+  "/",
+  checkAccountPayload,
+  checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const { name, budget } = req.body;
+      const trimmedName = name.trim();
+      const newAccount = await Accounts.create({ name: trimmedName, budget });
+      res.status(201).json(newAccount);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-router.put("/:id", (req, res, next) => {
-  // DO YOUR MAGIC
-});
+router.put(
+  "/:id",
+  checkAccountId,
+  checkAccountPayload,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { name, budget } = req.body;
+      const trimmedName = name.trim();
+      const updatedAccount = await Accounts.updateById(id, {
+        name: trimmedName,
+        budget,
+      });
+      res.json(updatedAccount);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-router.delete("/:id", (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete("/:id", checkAccountId, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedAccount = await Accounts.deleteById(id);
+    res.json(deletedAccount);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.use((err, req, res, next) => {
-  // eslint-disable-line
-  // DO YOUR MAGIC
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    return res.status(400).json({ message: "Invalid JSON payload" });
+  }
+
+  res.status(500).json({ message: "Something went wrong" });
 });
 
 module.exports = router;
