98d8729ef777c5d356f44fe1575dddaa638b585b
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-11 13:14:54 -0700

    wip

--- queries.sql
+++ queries.sql
@@ -1,11 +1,11 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
-
+SELECT * FROM [Customers] WHERE postalcode = '1010';
 -- Find the phone number for the supplier with the id 11
-
+SELECT Phone FROM [Suppliers] WHERE SupplierID = 11;
 -- List first 10 orders placed, sorted descending by the order date
-
+SELECT * FROM [Orders] ORDER BY OrderDate DESC LIMIT 10;
 -- Find all customers that live in London, Madrid, or Brazil
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
