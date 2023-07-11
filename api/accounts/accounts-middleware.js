a1f5f190d747c8a5a98d006bad9ce0ba5227e993
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-11 13:44:02 -0700

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,24 @@
+const yup = require("yup");
+const Account = require("./accounts-model");
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
-}
+  const accountSchema = yup.object().shape({
+    name: yup.string().required().trim().min(3).max(100),
+    budget: yup.number().required().positive().max(1000000),
+  });
+
+  try {
+    accountSchema.validateSync(req.body);
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
